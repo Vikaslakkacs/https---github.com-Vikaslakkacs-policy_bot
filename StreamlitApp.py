cd98af04f0bb69f114432ae4c8a11bdf95347a1d
Vikas Lakka <vikaslakkacs@gmail.com>
2024-02-11 23:09:44 +0530

    chat

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -30,16 +30,12 @@ with st.sidebar:
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
-    '''# Display chat messages from history on app rerun
+    # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
-            st.markdown(message["content"])'''
+            st.markdown(message["content"])
 
     if question := st.chat_input("Ask something"):
-        # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
         messages.chat_message("user").write(question)
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": question})
