7b3d7c9db44dbcd72706ca5064695b6d4a64d45a
Vikas Lakka <vikaslakkacs@gmail.com>
2024-02-11 23:13:35 +0530

    chat history

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -31,7 +31,8 @@ with st.sidebar:
     if "messages" not in st.session_state:
         st.session_state.messages = []
     # Display chat messages from history on app rerun
-    st.text("History")
+    if st.session_state.messages != []:
+        st.text("Chat-history")
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
