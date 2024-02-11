73e18db8450f27c255fe6e7bf26bc6451877c6bb
Vikas Lakka <vikaslakkacs@gmail.com>
2024-02-11 22:44:50 +0530

    chat

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -25,7 +25,7 @@ with st.form("user_inputs"):
     ask_button= st.form_submit_button("Ask")
 ###########################################
 with st.sidebar:
-    messages = st.container(height=600)
+    messages = st.container(height=400)
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -38,7 +38,7 @@ with st.sidebar:
     if question := st.chat_input("Say something"):
         messages.chat_message("user").write(question)
         # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": question})
+        messages.session_state.messages.append({"role": "user", "content": question})
         ##messages.chat_message("assistant").write(f"Echo: {prompt}")
 ###########################################
 ## Field Validations
@@ -76,4 +76,4 @@ if upload_file_list is not None and question:
                     # Display assistant response in chat message container
                 messages.chat_message("assistant").write(f"Bot: {response.get('policy_response')}")
                 # Add assistant response to chat history
-                st.session_state.messages.append({"role": "assistant", "content": response.get('policy_response')})
+                messages.session_state.messages.append({"role": "assistant", "content": response.get('policy_response')})
