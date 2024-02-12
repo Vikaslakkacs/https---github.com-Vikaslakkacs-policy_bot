10a886edeab2ca0c4593ddbd7a80ce7ebdc490e6
Vikas Lakka <41446834+Vikaslakkacs@users.noreply.github.com>
2024-02-12 07:25:16 +0000

    char history only atleast two questions asked.

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -75,9 +75,9 @@ with st.sidebar:
     if "messages" not in st.session_state:
         st.session_state.messages = []
     # Display chat messages from history on app rerun
-    if st.session_state.messages != []:
+    if len(st.session_state.messages) >2:
         st.text("Chat-history")
         
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
